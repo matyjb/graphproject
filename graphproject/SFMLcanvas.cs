7bdc6fce40ca66f9dfe8c434f03902dbf724b97b
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2018-04-27 19:26:34 +0200

    rysowanie przykładowego grafu

--- graphproject/SFMLcanvas.cs
+++ graphproject/SFMLcanvas.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows.Forms;
 using SFML.Graphics;
+using SFML.System;
 
 namespace graphproject
 {
     public partial class SFMLcanvas : UserControl
     {
         private RenderWindow RendWind;
+
+        private List<Vert> wierzcholkiList = new List<Vert>();
+
+        //przykladowy graf
+        private int[,] graf = { {0, 1, 1, 1}, 
+                                {1, 0, 0, 1}, 
+                                {1, 0, 0, 1}, 
+                                {1, 1, 1, 0}};
+
+        //resources
+        private Text text = new Text("0", new Font("arial.ttf"));
+        private CircleShape circle = new CircleShape(20)
+        {
+            FillColor = Color.Black,
+            Origin = new Vector2f(17.5f,11.5f),
+            OutlineThickness = 4,
+            OutlineColor = Color.White
+        };
 
         public SFMLcanvas()
         {
@@ -16,16 +36,38 @@ namespace graphproject
 
         public void StartSLMF()
         {
+            Random rnd = new Random();
             renderLoopWorker.RunWorkerAsync(Handle);
+            for (int i = 1; i <= graf.GetLength(0); i++)
+            {
+                var p = new Vert(circle, text, new Vector2f(rnd.Next(20,Width-20), rnd.Next(20, Height - 20)), i);
+                wierzcholkiList.Add(p);
+            }
         }
 
         private void DrawStuff()
         {
-            var CS = new CircleShape(10)
+            //rysowanie linii łączących
+            for (int i = 0; i < graf.GetLength(0); i++)
             {
-                FillColor = Color.Magenta
-            };
-            RendWind.Draw(CS);
+                for (int j = 0; j < graf.GetLength(1); j++)
+                {
+                    if (i != j && graf[i,j] == 1)
+                    {
+                        Vertex[] lines = new Vertex[2];
+                        lines[0] = new Vertex(wierzcholkiList[i].Position);
+                        lines[1] = new Vertex(wierzcholkiList[j].Position);
+
+                        RendWind.Draw(lines,PrimitiveType.Lines);
+                    }
+                }
+            }
+
+            //rysowanie wierzcholkow
+            foreach (var itemVert in wierzcholkiList)
+            {
+                RendWind.Draw(itemVert);
+            }
         }
 
         private void RenderLoopWorker_DoWork(object sender, DoWorkEventArgs e)
@@ -34,7 +76,7 @@ namespace graphproject
             while (RendWind.IsOpen)
             {
                 RendWind.DispatchEvents();
-                RendWind.Clear(new Color(51, 77, 102));
+                RendWind.Clear(Color.Black);
 
                 DrawStuff();
 
---
+++ graphproject/Vert.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+
+namespace graphproject
+{
+    internal class Vert : Transformable, Drawable
+    {
+        private readonly CircleShape circleShape;
+        private readonly Text text;
+        private readonly int number;
+
+        public Vert(CircleShape circle, Text text, Vector2f position, int number)
+        {
+            circleShape = circle;
+            Position = position;
+            this.number = number;
+            this.text = text;
+        }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            circleShape.Position = Position;
+            text.DisplayedString = number.ToString();
+            text.Origin = new Vector2f(text.GetLocalBounds().Width/2.0f, text.GetLocalBounds().Height / 2.0f);
+            text.Position = Position;
+
+            target.Draw(circleShape, states);
+            target.Draw(text, states);
+        }
+    }
+}
