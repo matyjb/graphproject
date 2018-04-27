bac1e51a6af4bc5c397c9d6ba7a231df712d2270
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2018-04-27 15:18:44 +0200

    okienko sfmla w środku windows forms

---
+++ graphproject/SFMLcanvas.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using SFML.Graphics;
+
+namespace graphproject
+{
+    public partial class SFMLcanvas : UserControl
+    {
+        private RenderWindow RendWind;
+
+        public SFMLcanvas()
+        {
+            InitializeComponent();
+        }
+
+        public void StartSLMF()
+        {
+            renderLoopWorker.RunWorkerAsync(Handle);
+        }
+
+        private void DrawStuff()
+        {
+            var CS = new CircleShape(10)
+            {
+                FillColor = Color.Magenta
+            };
+            RendWind.Draw(CS);
+        }
+
+        private void RenderLoopWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            RendWind = new RenderWindow((IntPtr) e.Argument);
+            while (RendWind.IsOpen)
+            {
+                RendWind.DispatchEvents();
+                RendWind.Clear(new Color(51, 77, 102));
+
+                DrawStuff();
+
+                RendWind.Display();
+            }
+        }
+
+        private void SFMLcanvas_Load(object sender, EventArgs e)
+        {
+            StartSLMF();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (RendWind == null)
+                base.OnPaint(e);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs pevent)
+        {
+            if (RendWind == null)
+                base.OnPaintBackground(pevent);
+        }
+    }
+}
