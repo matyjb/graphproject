4ad10248dc2bb993efc57928fa028c6e2e71e577
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2018-06-04 22:26:45 +0200

    konsolowa apka do testow

---
+++ EdmondsKarpTest/GraphGenerator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace graphproject
+{
+    public class GraphGenerator
+    {
+        public static int[,] Generate(int vcount,bool directed, bool mixed = true)
+        {
+            int[,] result = new int[0,0];
+            do
+            {
+                if (directed && mixed) result = GenerateDirectedMixed(vcount);
+                if (directed && !mixed) result = GenerateDirectedNonMixed(vcount);
+                if (!directed) result = GenerateNonDirected(vcount);
+            } while (!GraphConsistency.CheckConsistency(result));
+            return result;
+        }
+
+        private static int[,] GenerateNonDirected(int vcount)
+        {
+            int[,] result = new int[vcount,vcount];
+            //40% szans na krawędz
+            Random rnd = new Random();
+
+            for (int i = 0; i < vcount; i++)
+            {
+                for (int j = i+1; j < vcount; j++)
+                {
+                    if (rnd.Next(0, 10) < 4)
+                    {
+                        result[i, j] = result[j, i] = rnd.Next(20);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static int[,] GenerateDirectedMixed(int vcount)
+        {
+            Random rnd = new Random();
+
+            int[,] result = GenerateNonDirected(vcount);
+
+            for (int i = 0; i < vcount; i++)
+            {
+                for (int j = i + 1; j < vcount; j++)
+                {
+                    if (result[i, j] != 0)
+                    {
+                        int los = rnd.Next(0, 6);
+                        if (los == 0) result[i, j] = rnd.Next(20);
+                        if (los == 1) result[j, i] = rnd.Next(20);
+                        if (los == 4) result[i, j] = 0;
+                        if (los == 5) result[j, i] = 0;
+                    }
+                }
+            }
+            return result;
+        }
+        private static int[,] GenerateDirectedNonMixed(int vcount)
+        {
+            Random rnd = new Random();
+
+            int[,] result = GenerateNonDirected(vcount);
+
+            for (int i = 0; i < vcount; i++)
+            {
+                for (int j = i + 1; j < vcount; j++)
+                {
+                    if (result[i, j] != 0)
+                    {
+                        int los = rnd.Next(0, 2);
+                        if (los == 0) result[i, j] = 0;
+                        if (los == 1) result[j, i] = 0;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ EdmondsKarpTest/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace graphproject
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string wyjscie = "";
+            do
+            {
+
+                string log = string.Format("{0,23}|{1,23}|{2,23}|{3,23}", "Max Flow", "Czas wykonania(ticks)", "Ilość wierzchołków", "Ilość dróg(BFS)");
+                Console.Write("podaj ilosc testu: ");
+                int n = Convert.ToInt16(Console.ReadLine());
+                Random rnd = new Random();
+                Console.WriteLine(log);
+                for (int i = 0; i < n; i++)
+                {
+                    int[,] graf = GraphGenerator.Generate(rnd.Next(4, 150), true);
+                    EdmondsKarp ek = new EdmondsKarp();
+                    int sink = 0, source = 0;
+                    while (sink == source)
+                    {
+                        sink = rnd.Next(0, graf.GetLength(0));
+                        source = rnd.Next(0, graf.GetLength(0));
+                    }
+                    Stopwatch c = Stopwatch.StartNew();
+                    int mf = ek.FindMaxFlow(graf, NeighborsList(graf), source, sink, out var lf);
+                    c.Stop();
+                    int k = ek.k;
+                    string nowywpis = string.Format("{0,23}|{1,23}|{2,23}|{3,23}", mf, c.ElapsedTicks, graf.GetLength(0),
+                        k);
+                    log += Environment.NewLine + nowywpis;
+                    Console.WriteLine(nowywpis);
+                }
+                string odp = "";
+                do
+                {
+                    Console.Write("czy chcesz zapisać wynik do pliku?[T/N] ");
+                    odp = Console.ReadLine();
+                } while (odp.ToLower() != "n" && odp.ToLower() != "t");
+                if (odp.ToLower() == "t")
+                {
+                    SaveFileDialog sfd = new SaveFileDialog();
+
+                    sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    sfd.FilterIndex = 2;
+                    sfd.RestoreDirectory = true;
+
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        StreamWriter stream = new StreamWriter(sfd.FileName);
+                        stream.WriteLine(log);
+                        stream.Close();
+                    }
+                }
+                do
+                {
+                    Console.Write("Czy zamnknąć program?[T/N]");
+                    wyjscie = Console.ReadLine();
+                } while (wyjscie.ToLower() != "n" && wyjscie.ToLower() != "t");
+            } while (wyjscie.ToLower() == "n");
+        }
+        static Dictionary<int, List<int>> NeighborsList(int[,] graf)
+        {
+            Dictionary<int, List<int>> result = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < graf.GetLength(0); i++)
+            {
+                List<int> list = new List<int>();
+                for (int j = 0; j < graf.GetLength(1); j++)
+                {
+                    if (graf[i, j] != 0)
+                    {
+                        list.Add(j);
+                    }
+                }
+                result.Add(i, list);
+            }
+
+            return result;
+        }
+    }
+}
